3142d8f0a0dca43efe75ac8636b448df95a76814
7db9a <repo.7db9a@gmail.com>
2020-04-21 14:31:11 -0400

    Saving progress.

--- src/bitcoin/mod.rs
+++ src/bitcoin/mod.rs
@@ -6,9 +6,13 @@ mod  bitcoin_integration {
     use super::{sv,};
     use sv::wallet;
 
+
    #[test]
    fn mnemonic_to_xpriv() {
-       wallet::mnemonic_decode;
+       let w = wallet::Wordlist::English;
+       let wordlist = wallet::load_wordlist(w);
+       let m = ["legal".to_string(), "winner".to_string(), "thank".to_string(), "year".to_string(), "wave".to_string(), "sausage".to_string(), "worth".to_string(), "useful".to_string(), "legal".to_string(), "winner".to_string(), "thank".to_string(), "yellow".to_string()];
+       wallet::mnemonic_decode(&m, &wordlist).unwrap();
        assert_eq!(true, false);
    }
 }
