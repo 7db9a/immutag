49d7722fe1c33bcfc775a9ea92c254c0e7a82607
7db9a <repo.7db9a@gmail.com>
2020-04-21 14:49:39 -0400

    Add master private key function.

--- src/bitcoin/mod.rs
+++ src/bitcoin/mod.rs
@@ -1,10 +1,31 @@
 extern crate sv;
-
+extern crate ring;
+extern crate hex;
 
 #[cfg(test)]
 mod  bitcoin_integration {
-    use super::{sv,};
+    use super::{sv, ring, hex};
     use sv::wallet;
+    use wallet::ExtendedKey;
+    use sv::network::Network;
+    use ring::digest::SHA512;
+    use ring::hmac;
+
+    fn master_private_key(seed: &str) -> ExtendedKey {
+        let seed = hex::decode(seed).unwrap();
+        let key = "Bitcoin seed".to_string();
+        let key = hmac::SigningKey::new(&SHA512, &key.as_bytes());
+        let hmac = hmac::sign(&key, &seed);
+        ExtendedKey::new_private_key(
+            Network::Mainnet,
+            0,
+            &[0; 4],
+            0,
+            &hmac.as_ref()[32..],
+            &hmac.as_ref()[0..32],
+        )
+        .unwrap()
+    }
 
 
    #[test]
@@ -13,6 +34,7 @@ mod  bitcoin_integration {
        let wordlist = wallet::load_wordlist(w);
        let m = ["legal".to_string(), "winner".to_string(), "thank".to_string(), "year".to_string(), "wave".to_string(), "sausage".to_string(), "worth".to_string(), "useful".to_string(), "legal".to_string(), "winner".to_string(), "thank".to_string(), "yellow".to_string()];
        wallet::mnemonic_decode(&m, &wordlist).unwrap();
+
        assert_eq!(true, false);
    }
 }
