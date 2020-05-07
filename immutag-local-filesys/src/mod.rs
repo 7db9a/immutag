8d59cacd0966c01e78a7949af4cd79b210db50cc
7db9a <repo.7db9a@gmail>
2020-05-07 18:51:34 -0400

    Saving progress.

--- immutag-local-filesys/src/mod.rs
+++ immutag-local-filesys/src/mod.rs
@@ -1,6 +1,5 @@
 use fixture::{Fixture};
 
-
 fn path_filesys<T: AsRef<str>>(immutag_file_path: T, bitcoin_addr: T) -> String {
     let bitcoin_addr = fixture::directorate(bitcoin_addr.as_ref().to_string());
     let path = fixture::directorate(immutag_file_path.as_ref().to_string());
