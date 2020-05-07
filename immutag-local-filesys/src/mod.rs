1bceac5b628e7839d16dd8607eee6c07e9080ebc
7db9a <repo.7db9a@gmail>
2020-05-07 19:08:29 -0400

    tests: fail to add-filsys directory.

--- immutag-local-filesys/src/mod.rs
+++ immutag-local-filesys/src/mod.rs
@@ -1,4 +1,4 @@
-use fixture::{Fixture};
+use fixture::Fixture;
 
 fn path_filesys<T: AsRef<str>>(immutag_file_path: T, bitcoin_addr: T) -> String {
     let bitcoin_addr = fixture::directorate(bitcoin_addr.as_ref().to_string());
@@ -8,15 +8,39 @@ fn path_filesys<T: AsRef<str>>(immutag_file_path: T, bitcoin_addr: T) -> String 
     path
 }
 
+fn mkdir_filesys<T: AsRef<str>>(path: T) {
+    let mut fixture = Fixture::new()
+       .add_dirpath(path.as_ref().to_string())
+       .build();
+}
+
+fn add_filesys<T: AsRef<str>>(immutag_file_path: T, bitcoin_addr: T) {
+    let path = path_filesys(immutag_file_path, bitcoin_addr);
+    mkdir_filesys(path);
+}
+
+#[cfg(test)]
+mod tests {
+    fn path_filesys() {
+        let path = super::path_filesys("/tmp/immutag_test/.immutag", "1LrTstQYNZj8wCvBgipJqL9zghsofpsHEG");
+
+        assert_eq!(
+            path,
+            "/tmp/immutag_test/.immutag/1LrTstQYNZj8wCvBgipJqL9zghsofpsHEG/".to_string()
+        );
+    }
+}
+
 #[cfg(test)]
 mod integration {
+    use std::path::Path;
 
     #[test]
-    fn path_filesys() {
-        let filesys_path = super::path_filesys("/tmp/immutag_test/.immutag", "1LrTstQYNZj8wCvBgipJqL9zghsofpsHEG");
+    fn add_filesys() {
+        super::add_filesys("/tmp/immutag_test/.immutag", "1LrTstQYNZj8wCvBgipJqL9zghsofpsHEG");
+
         assert_eq!(
-            filesys_path,
-            "/tmp/immutag_test/.immutag/1LrTstQYNZj8wCvBgipJqL9zghsofpsHEG/".to_string()
-        )
+           Path::new("/tmp/immutag_test/.immutag/1LrTstQYNZj8wCvBgipJqL9zghsofpsHEG/").exists()
+           , false);
     }
 }
