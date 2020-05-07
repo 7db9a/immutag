ce9e4845dc4c3fea09593cbaa60d35ecfec9ce00
7db9a <repo.7db9a@gmail>
2020-05-07 18:22:06 -0400

    Saving progress.

--- immutag-local-filesys/src/mod.rs
+++ immutag-local-filesys/src/mod.rs
@@ -1,10 +1,14 @@
 use fixture;
 
+fn mkdir_filesys() {
+}
+
 #[cfg(test)]
 mod integration {
 
     #[test]
     fn mkdir_filesys() {
+        super::mkdir_filesys();
         assert_eq!(true, false)
     }
 }
