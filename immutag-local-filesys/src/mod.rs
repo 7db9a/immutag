3edec8e8399688b0f079ad4d9828a6088b8bb955
7db9a <repo.7db9a@gmail>
2020-05-07 18:50:53 -0400

    Saving progress.

--- immutag-local-filesys/src/mod.rs
+++ immutag-local-filesys/src/mod.rs
@@ -1,7 +1,7 @@
 use fixture::{Fixture};
 
 
-fn mkdir_filesys<T: AsRef<str>>(immutag_file_path: T, bitcoin_addr: T) -> String {
+fn path_filesys<T: AsRef<str>>(immutag_file_path: T, bitcoin_addr: T) -> String {
     let bitcoin_addr = fixture::directorate(bitcoin_addr.as_ref().to_string());
     let path = fixture::directorate(immutag_file_path.as_ref().to_string());
     let path = fixture::directorate(path + bitcoin_addr.as_ref());
@@ -13,8 +13,8 @@ fn mkdir_filesys<T: AsRef<str>>(immutag_file_path: T, bitcoin_addr: T) -> String
 mod integration {
 
     #[test]
-    fn mkdir_filesys() {
-        let filesys_path = super::mkdir_filesys("/tmp/immutag_test/.immutag", "1LrTstQYNZj8wCvBgipJqL9zghsofpsHEG");
+    fn path_filesys() {
+        let filesys_path = super::path_filesys("/tmp/immutag_test/.immutag", "1LrTstQYNZj8wCvBgipJqL9zghsofpsHEG");
         assert_eq!(
             filesys_path,
             "/tmp/immutag_test/.immutag/1LrTstQYNZj8wCvBgipJqL9zghsofpsHEG/".to_string()
