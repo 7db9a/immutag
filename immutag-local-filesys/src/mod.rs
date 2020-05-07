4e4fc43a087c9dab0dcc3907b5d00def1202bd6b
7db9a <repo.7db9a@gmail>
2020-05-07 18:50:02 -0400

    Saving progress.

--- immutag-local-filesys/src/mod.rs
+++ immutag-local-filesys/src/mod.rs
@@ -14,10 +14,10 @@ mod integration {
 
     #[test]
     fn mkdir_filesys() {
-        let filesys_path = super::mkdir_filesys("tmp", "1LrTstQYNZj8wCvBgipJqL9zghsofpsHEG");
+        let filesys_path = super::mkdir_filesys("/tmp/immutag_test/.immutag", "1LrTstQYNZj8wCvBgipJqL9zghsofpsHEG");
         assert_eq!(
             filesys_path,
-            "/tmp/immutag_test/.immutag/1LrTstQYNZj8wCvBgipJqL9zghsofpsHEG".to_string()
+            "/tmp/immutag_test/.immutag/1LrTstQYNZj8wCvBgipJqL9zghsofpsHEG/".to_string()
         )
     }
 }
